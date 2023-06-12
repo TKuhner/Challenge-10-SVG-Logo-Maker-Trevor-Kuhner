c269ed7e02891275bb16ac8b4efa93e97cbe42f6
TKuhner <trevorkuhner29@gmail.com>
2023-06-12 17:42:58 -0500

    added shape examples to folder structure

---
+++ index.js
@@ -0,0 +1,7 @@
+// prompt user for txt (up to 3 chars)
+// prompt user for color of text
+// prompt user for shape
+// prompt user for color of shape
+// take user input and render svg
+// print 'generated logo.svg' to console
+// open logo.svg in browser shows logo
